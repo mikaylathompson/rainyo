e220e9df6bd68414b1101d460ca6d20a08e525d5
Mikayla <mikayla.thompson@yale.edu>
2014-07-23 15:33:37 +0000

    requests, posts, jsons

--- rainyo.py
+++ rainyo.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/madison', methods = ['POST'])
 def rainyomadison():
-    if request.json["raining"] == "true":
+    if request.get_json(force=True)["raining"] == "true":
         r = requests.post("http://api.justyo.co/yoall/", data={'api_token': '3e795a3c-e627-1eef-41bb-cd7b0aee5f79'})
         return "Yodeled.  Or at least you tried to."
     else:
