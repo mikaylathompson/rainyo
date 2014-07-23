ec15e56dd9b0f6eec29a2874950e084ed62b5fb9
Mikayla <mikayla.thompson@yale.edu>
2014-07-23 15:29:15 +0000

    No yo button, but better POST methods

--- rainyo.py
+++ rainyo.py
@@ -2,37 +2,26 @@ import os
 import requests
 import json
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Want to know when it's raining in Madison?  Send a yo to MADISONRAINYO"
+    return "Want to know when it's raining in Madison?  Send a yo to MadisonRainYo"
 
 @app.route('/madison', methods = ['POST'])
 def rainyomadison():
-    r = requests.post("http://api.justyo.co/yoall/", data={'api_token': '3e795a3c-e627-1eef-41bb-cd7b0aee5f79'})
-    return "Yodeled.  Or at least you tried to."
-
-yo_button = """<head><title>Madison RainYo</title></head>
-    <body>
-    <script type="text/javascript">
-    	var _yoData = {
-	    	"username": "MADISONRAINYO",
-	    	"trigger": "it's raining in Madison"
-	    };
-	    var s = document.createElement("script");
-	    s.type = "text/javascript";
-    	s.src = "//yoapp.s3.amazonaws.com/js/yo-button.js";
-	    (document.head || document.getElementsByTagName("head")[0]).appendChild(s);
-    </script>
-    <div id="yo-button"></div>
-    </body>"""
+    if request.json["raining"] == "true":
+        r = requests.post("http://api.justyo.co/yoall/", data={'api_token': '3e795a3c-e627-1eef-41bb-cd7b0aee5f79'})
+        return "Yodeled.  Or at least you tried to."
+    else:
+        return "I'm not going to yo unless it's raining."
 
 
 @app.route('/madison')
 def visit_madison():
-    return yo_button
+    return "Yo, want to know when it's raining in Madison?  Send a yo to MadisonRainYo."
 
 
 if __name__ == "__main__":
