4b1e60ad0ee7e79a9ffd0a5a02eb701081f29e66
Mikayla <mikayla.thompson@yale.edu>
2014-07-23 15:50:22 +0000

    requests, posts, jsons

--- rainyo.py
+++ rainyo.py
@@ -12,27 +12,16 @@ def index():
 
 @app.route('/madison', methods = ['POST'])
 def rainyomadison():
-    try:
-        x = json.loads(request.data)
-        try:
-            if x["raining"] == "true":
-                return "It's raining, and I read the request."
-            elif x["raining"] == "false":
-                return "It's not raining, but I read the request."
-            else:
-                return "I don't know if it's raining, but I think I read the request."
-        except Exception as e:
-            return "Error: ", e
-    except Exception as e:
-        return "Error (early): ", e
+    x = json.loads(request.data)
+    if x["raining"] == "true":
+        r = requests.post("http://api.justyo.co/yoall/", data={'api_token': '3e795a3c-e627-1eef-41bb-cd7b0aee5f79'})
+        return "Yodeled.  Or at least you tried to."
+    elif x["raining"] == "false":
+        return "It's not raining, so I'm not going to yo."
+    else:
+        return "Something's not right."
                 
                 
-#    if json.loads(request.data)["raining"] == "true":
-#        r = requests.post("http://api.justyo.co/yoall/", data={'api_token': '3e795a3c-e627-1eef-41bb-cd7b0aee5f79'})
-#        return "Yodeled.  Or at least you tried to."
-#    else:
-#        return "I'm not going to yo unless it's raining."
-
 
 @app.route('/madison')
 def visit_madison():
